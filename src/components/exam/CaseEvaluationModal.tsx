565ca2bbcec2d13e929cc5e6dee28c5b945958a9
Maxty9378 <maxim.kadochkin@gmail.com>
2025-09-17 22:15:01 +0300

    Исправлен двойной safe-area отступ в CaseEvaluationModal - убран paddingBottom из основного контейнера для устранения конфликта с футером

--- src/components/exam/CaseEvaluationModal.tsx
+++ src/components/exam/CaseEvaluationModal.tsx
@@ -449,7 +449,6 @@ export const CaseEvaluationModal: React.FC<CaseEvaluationModalProps> = ({
           paddingTop: 'env(safe-area-inset-top, 0px)',
           paddingLeft: 'env(safe-area-inset-left, 0px)',
           paddingRight: 'env(safe-area-inset-right, 0px)',
-          paddingBottom: 'env(safe-area-inset-bottom, 0px)',
           transform: 'translate3d(0, 0, 0)',
           backfaceVisibility: 'hidden',
           willChange: 'transform',
