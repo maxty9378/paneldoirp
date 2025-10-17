2d11db531db7b412c971aaeac776181ca22ff0a2
maxty9378 <doirp.sns777@gmail.com>
2025-10-17 17:23:15 +0300

    fix: добавлены заголовки accept-profile и apikey в CORS allowedHeaders

--- bff/src/middleware/cors.ts
+++ bff/src/middleware/cors.ts
@@ -20,7 +20,7 @@ export const corsMiddleware = cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Request-ID', 'x-client-info'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Request-ID', 'x-client-info', 'accept-profile', 'apikey'],
   exposedHeaders: ['X-Request-ID'],
 });
 
